671fbebb2305496b8987cc08c5fe050385a82ad7
Enzo Matías Nieto <enzonieto92@hotmail.com>
2023-07-23 22:45:06 -0300

    Fixes de Cámara

--- personaje.gd
+++ personaje.gd
@@ -11,14 +11,14 @@ var just_landed := is_on_floor() and _snap_vector == Vector3.DOWN
 var direction := Vector3.ZERO
 var angle_x := 0.0
 var angle2 := 0.0
-
+var last_rotation = Vector3.ZERO
+var rotated_direction := Vector3.ZERO
 func _physics_process(delta: float) -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	var is_jumping := is_on_floor() and Input.is_key_pressed(KEY_SPACE)
-	var rotated_direction = direction.rotated(Vector3.UP, angle2)
 	speed = 4
 	direction = Vector3.ZERO
-
+	
 	if Input.is_key_pressed(KEY_A):
 		direction.x = -1
 	elif Input.is_key_pressed(KEY_D):
@@ -29,21 +29,23 @@ func _physics_process(delta: float) -> void:
 		direction.z = 1
 	if Input.is_key_pressed(KEY_SHIFT):
 		speed = 15
+	rotated_direction = direction.rotated(Vector3.UP, angle2)
 
 	velocity.x = rotated_direction.x * speed
 	velocity.z = rotated_direction.z * speed
 	velocity.y -= gravity * delta
 
 	rotate(Vector3.UP, angle_x)
-
-	if direction != Vector3.ZERO:
-		camera_arm.top_level = true
+	camera_arm.top_level = true
+	if direction == Vector3.ZERO:
+		rotation = last_rotation
+	else:
 		rotation.y = atan2(-rotated_direction.x, -rotated_direction.z)
-		camera_arm.top_level = false
+		last_rotation = rotation
+	camera_arm.top_level = false
 	move_and_slide()
 	angle2 += angle_x
 	angle_x = 0.0
-
 	if is_jumping:
 		velocity.y += salto
 		_snap_vector = Vector3.UP
@@ -56,17 +58,15 @@ func _physics_process(delta: float) -> void:
 		state_machine.travel("Walk")
 	else:
 		state_machine.travel("Idle")
-
+	
 func _unhandled_input(event):
 	if event is InputEventMouseMotion:
-
-		if Input.is_mouse_button_pressed(MOUSE_BUTTON_RIGHT):
-			angle_x = event.relative.x * -0.002
-			camera_arm.rotation.x += event.relative.y * -0.002
-			if camera_arm.rotation.x >= 0.8:
-				camera_arm.rotation.x = 0.8
-			elif camera_arm.rotation.x <= -0.8:
-				camera_arm.rotation.x = -0.8
+		angle_x = event.relative.x * -0.002
+		camera_arm.rotation.x += event.relative.y * -0.002
+		if camera_arm.rotation.x >= 1:
+			camera_arm.rotation.x = 1
+		elif camera_arm.rotation.x <= -1:
+			camera_arm.rotation.x = -1
 
 
 	if event is InputEventKey:
